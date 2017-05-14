6674fb6fbba34882e78fa94b20443d9c08780a36
bart-rijnders <mail@bartrijnders.com>
2017-05-14 18:03:05 +0200

    Added wordlist

---
+++ words/convert_to_json.py
@@ -0,0 +1,13 @@
+import json
+
+f = open('wordlist.txt', 'r')
+words = [line.rstrip() for line in f if len(line) > 2]
+
+def write_words(filename, amount):
+   file = open(filename, 'w+')
+   file.write(json.dumps(words[0:amount]))
+   file.close()
+
+write_words("top_100_words.json", 100)
+write_words("top_1000_words.json", 1000)
+write_words("top_10000_words.json", 10000)
